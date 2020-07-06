bafb596934c6413ab34541cea1e9aacb76b6754c
julzar <56980945+julzar@users.noreply.github.com>
2020-07-05 20:56:13 -0500

    Update App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Redirect,
 } from 'react-router-dom'
 
-// import { Navbar } from './components/Navbar'
-// import DashboardPage from './pages/DashboardPage'
-// import PostsPage from './pages/PostsPage'
-// import SinglePostPage from './pages/SinglePostPage'
 import EmployeesPage from './pages/EmployeesPage'
 
 const App = () => {
   return (
     <Router>
-      {/* <Navbar /> */}
-      <Switch>
         <Route exact path="/" component={EmployeesPage} />
-        {/* <Route exact path="/posts" component={PostsPage} /> */}
-        {/* <Route exact path="/posts/:id" component={SinglePostPage} /> */}
-        <Redirect to="/" />
-      </Switch>
     </Router>
   )
 }
