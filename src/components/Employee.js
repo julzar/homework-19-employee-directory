c6e459ac69b09d818de1611dad8abf2f9f48d864
julzar <56980945+julzar@users.noreply.github.com>
2020-07-05 20:57:36 -0500

    update Employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,32 +1,19 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 
-// export const Post = ({ post, excerpt }) => (
-//   <article className={excerpt ? 'post-excerpt' : 'post'}>
-//     <h2>{post.title}</h2>
-//     <p>{excerpt ? post.body.substring(0, 100) : post.body}</p>
 
-//     {excerpt && (
-//       <Link to={`/posts/${post.id}`} className="button">
-//         View Post
-//       </Link>
-//     )}
-//   </article>
-// )
-
-export const Employee = ({ first, last, phone, email}) => (
+export const Employee = ({ employee }) => { return (
     <tr>
       <td>
-        {first}
+        {employee.name.first}
       </td>
       <td>
-        {last}
+        {employee.name.last}
       </td>
       <td>
-        {phone}
+        {employee.phone}
       </td>
       <td>
-        {email}
+        {employee.email}
       </td>
     </tr>
-)
+)}
